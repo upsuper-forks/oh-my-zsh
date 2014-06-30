f821c88061907c3b2c3340134c2295b86755a778
Xidorn Quan <quanxunzhen@gmail.com>
2014-06-30 12:14:00 +1000

    notify: use echo for unsupported as default.

--- plugins/notify/notify.plugin.zsh
+++ plugins/notify/notify.plugin.zsh
@@ -6,6 +6,14 @@
 # Usage:
 # notify message [title]
 
+function _notify-echo() {
+  local msg
+  msg=""
+  [[ "x$2" != "x" ]] && msg+="$2: "
+  msg+="$1"
+  echo $msg
+}
+
 function _notify-terminal-notifier() {
   local args
   args=(-message "$1")
@@ -13,6 +21,7 @@ function _notify-terminal-notifier() {
   terminal-notifier $args >/dev/null 2>&1
 }
 
+alias notify=_notify-echo
 case `uname -s` in
   Darwin)
     if [[ $(whence terminal-notifier) != "" ]]; then
